74208d6ceac14c7007e1a81a76c6343cde66bda0
Leonardo Lemos <leolemox@gmail.com>
2020-04-06 16:00:51 -0300

    Adicionando configurações do JPA e do PostgreSQL e configurando docker-compose.yml

--- implementacao/ithappens-backend/src/test/java/br/com/ithappens/ithappensbackend/IthappensBackendApplicationTests.java
+++ implementacao/ithappens-backend/src/test/java/br/com/ithappens/ithappensbackend/IthappensBackendApplicationTests.java
@@ -1,9 +1,7 @@
 package br.com.ithappens.ithappensbackend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class IthappensBackendApplicationTests {
 
     @Test
