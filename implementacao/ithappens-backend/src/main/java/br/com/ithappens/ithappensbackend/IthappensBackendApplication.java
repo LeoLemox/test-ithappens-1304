f029f62ac246402444ffd3c380eb3d971403d79b
Leonardo Lemos <leolemox@gmail.com>
2020-04-06 13:34:23 -0300

    Inicializando projeto com Spring Boot

---
+++ implementacao/ithappens-backend/src/main/java/br/com/ithappens/ithappensbackend/IthappensBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.ithappens.ithappensbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IthappensBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IthappensBackendApplication.class, args);
+    }
+
+}
---
+++ implementacao/ithappens-backend/src/test/java/br/com/ithappens/ithappensbackend/IthappensBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ithappens.ithappensbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IthappensBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
